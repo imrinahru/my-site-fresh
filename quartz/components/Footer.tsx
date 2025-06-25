a756423e5528a1fcd86963e9bd24ca399dbff550
Rina Chen <rina.chen2016@gmail.com>
2025-06-25 18:08:03 +0900

    Quartz sync: Jun 25, 2025, 6:08 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,29 +1,22 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import style from "./styles/footer.scss"
-import { version } from "../../package.json"
-import { i18n } from "../i18n"
 
-interface Options {
-  links: Record<string, string>
-}
+
+interface Options {}
 
 export default ((opts?: Options) => {
   const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
-    const year = new Date().getFullYear()
-    const links = opts?.links ?? []
-    return (
+   return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          © {new Date().getFullYear()} Rina Chen. Some rights reserved.
         </p>
-        <ul>
-          {Object.entries(links).map(([text, link]) => (
-            <li>
-              <a href={link}>{text}</a>
-            </li>
-          ))}
-        </ul>
+        <p>
+          Content licensed under{" "}
+          <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank" rel="noopener noreferrer">
+            CC BY-NC-SA 4.0
+          </a>.
+        </p>
       </footer>
     )
   }
